afc96e544f8f0082edc0173a6f725124b7dd3dba
woojin <sun65568@hanmail.net>
2023-08-17 17:56:57 +0900

    코테 연습

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -4,15 +4,20 @@ public class SumAverage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] Array = new int[N];
-        int max = 0;
-        int avg = 0;
+        float[] Array = new float[N];
+        float M = 0;
+        float avg = 0;
         for(int i = 0; i < Array.length; i ++) { // 일단 최대값 구하기
-            Array[i] = sc.nextInt();
-            if(Array[i] > max) {
-                max = Array[i];
+            Array[i] = sc.nextFloat();
+            if(Array[i] > M) {
+                M = Array[i];
             }
         }
+        for(int j =0; j < Array.length; j++) {
+            avg += (Array[j]/M*100)/N;
+
+        }
+        System.out.println(avg);
     }
 
 }
