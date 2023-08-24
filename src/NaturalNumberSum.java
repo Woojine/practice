0ddeda6e0a6346677b4038901d4f8629c24355e4
woojin <sun65568@hanmail.net>
2023-08-25 00:54:53 +0900

    코테 연습

--- src/NaturalNumberSum.java
+++ src/NaturalNumberSum.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class NaturalNumberSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int A = sc.nextInt();
     }
 }
