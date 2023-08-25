b252ee698f66cb1d96e3e4d4df8369cc2721829e
woojin <sun65568@hanmail.net>
2023-08-25 16:28:08 +0900

    코테 연습

--- src/array/Array7.java
+++ src/array/Array7.java
@@ -1,7 +1,11 @@
 package array;
 
+import java.util.Scanner;
+
 public class Array7 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
     }
 }
