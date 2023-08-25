5a8d10040a4d38a78b7fb1e9eb84c5803ad0dc9f
woojin <sun65568@hanmail.net>
2023-08-25 17:18:35 +0900

    코테 연습

---
+++ src/array/Array9.java
@@ -0,0 +1,11 @@
+package array;
+
+import java.util.Scanner;
+
+public class Array9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+    }
+}
