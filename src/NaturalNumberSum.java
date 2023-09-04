3779420707b9a140f3d161b555aa835c3a050341
woojin <sun65568@hanmail.net>
2023-09-04 12:08:03 +0900

    코테 연습

--- src/NaturalNumberSum.java
+++ src/NaturalNumberSum.java
@@ -4,5 +4,6 @@ public class NaturalNumberSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
+        int B = sc.nextInt();
     }
 }
